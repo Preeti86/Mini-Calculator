91f856d8f4b2bdcefd1275b7e453c16cd8734451
preeti86 <preetijav@gmail.com>
2019-05-06 12:44:23 +0200

    code in

--- src/index.js
+++ src/index.js
@@ -1,12 +1,83 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Calculator extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+            output: "",
+        };
+    }
+        handleChange = event => {
+            let { value } = event.target;
+            value = Math.max(Number(min), Math.min(Number(max), Number(value)));
+
+            this.setState({ value });
+
+
+    }
+
+    render(){
+        let intA,intB,intC,intD;
+        return(
+            <div className="superContainer">
+                <div className="header">
+                    <header>
+                        <h1>Mini Calculator</h1>
+                    </header>
+                </div>
+                <div className="container">
+                    <div className="box1">
+                        <label>Value 1</label> <br/>
+                        <input placeholder="Value 1" type="text" value={this.state.value} onChange={this.handleChange} ref="Value1"/>
+                    </div>
+
+                    <div className="box2">
+                        <label >Value 2</label> <br/>
+                        <input placeholder="Value 2" type="text" value={this.state.value} onChange={this.handleChange} ref="Value2" />
+                    </div>
+
+
+                    <div className="box3">
+                        <label>Value 3</label> <br/>
+                        <input placeholder="Value 3" type="text" value={this.state.value} onChange={this.handleChange}  ref="Value3"  />
+                    </div>
+
+                    <div className="box4">
+                        <label><input className="value" value="" type="radio" name="radio"
+                                      checked={this.state.radio} onChange= { () => {
+                            intA = parseInt(ReactDOM.findDOMNode(this.refs.Value1).value)
+                            intB = parseInt(ReactDOM.findDOMNode(this.refs.Value2).value)
+                            intC = parseInt(ReactDOM.findDOMNode(this.refs.Value3).value)
+                            intD = intA + intB + intC
+                            this.setState({output:intD})
+                        }}/>&nbsp;<i className="glyphicon glyphicon-plus"/><span className="checkmark"/></label>
+                        <br/>
+                        <label>
+                            <input className="value" value="" type="radio" name="radio"
+                                   checked={this.state.radio} onChange={ () => {
+                                intA = parseInt(ReactDOM.findDOMNode(this.refs.Value1).value)
+                                intB = parseInt(ReactDOM.findDOMNode(this.refs.Value2).value)
+                                intC = parseInt(ReactDOM.findDOMNode(this.refs.Value3).value)
+                                intD = intA * intB * intC
+                                this.setState({output:intD})
+                            }}/>&nbsp;<i className="glyphicon glyphicon-asterisk"/><span className="checkmark"/></label>
+                        <br/>
+                        <h3>Result: {this.state.output}</h3>
+                    </div>
+                </div>
+            </div>
+
+        );
+    };
+}
+
+ReactDOM.render(
+    <Calculator/>,
+    document.getElementById('root')
+);
+
